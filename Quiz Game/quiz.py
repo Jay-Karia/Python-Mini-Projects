e6459450c0d064aff92b54520849dabca9299cf4
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-29 12:21:30 +0530

    Question 2 in "About World" Done!

--- Quiz Game/quiz.py
+++ Quiz Game/quiz.py
@@ -4,29 +4,39 @@ print("Welcome to Python Quiz Game Developed -- By Jay")
 topics = ["About World", "Current Affairs", "History", "Computer Science", "About Novels"]
 
 # Questions
-aboutWorldQ = ["Which one is the smallest ocean in the World?", "What happened during Earth's Dark Age",
-              " or the first 500 million years after it formed?", "How did life begin",
-              "How does Earth's interior work, and how does it affect the surface?",
-              "Why does Earth have plate tectonics and continents?"]
+aboutWorldQ = ["Which one is the smallest ocean in the World?", "Which country gifted the 'Statue of Liberty' to USA in 1886?",
+               " or the first 500 million years after it formed?", "How did life begin",
+               "How does Earth's interior work, and how does it affect the surface?",
+               "Why does Earth have plate tectonics and continents?"]
 
 # Answers
-aboutWorldA = ["Central Arctic Ocean"]
+aboutWorldA = ["Central Arctic Ocean", "French People"]
+
+points = 0
+
+
+# Methods
+def answer(*answers):
+    if answers == "Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean" or "France" or "france" or "French People" or "french people":
+        global points
+        points = + 1
+        print("Correct")
+        pass
+
 
 play = input("\nDo you want to play? ")
 
 if play == "Yes" or "yes":
-    points = 0
     select = input(
         "\nSelect a Topic: \n\n1) About World" "\n2) Current Affairs" "\n3) History" "\n4) Computer Science" "\n5) About Novels\n")
 
     if select == "1":
-        print("Questions ", topics[0], "\n")
-        for i in range(5):
-            print(f"\nQ-0{i}: ", aboutWorldQ[i])
+        print("Questions ", topics[0])
 
+        print("\nQ-1: ", aboutWorldQ[0])
         que1 = input()
-        if que1 == "Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean":
-            print("Correct")
-            point = + 1
-        else:
-            print("Answer: ", aboutWorldA[0])
+        answer("Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean")
+
+        print("\nQ-2: ", aboutWorldQ[1])
+        que2 = input()
+        answer("France" or "france" or "French People" or "french people")
