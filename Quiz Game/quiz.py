939e81698ff2b7542004d4138695ed7931cd9db4
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-29 14:49:22 +0530

    update quiz.py (2)

--- Quiz Game/quiz.py
+++ Quiz Game/quiz.py
@@ -12,21 +12,35 @@ aboutWorldQ = ["Which one is the smallest ocean in the World?",
                "In which ocean 'Bermuda Triangle' region is located?", "Which country is known as playground of europe"]
 
 # Answers
-aboutWorldA = ["central arctic Ocean", "france", "israel and jordan", "north atlantic ocean", "switzerland"]
+aboutWorldA = ["central arctic ocean", "france", "israel and jordan", "north atlantic ocean", "switzerland"]
 
 points = 0
 
 
 # Methods
 def answer(answers):
-    if answers.lower() == aboutWorldA[0] or answers.lower() == aboutWorldA[1] or answers.lower() == aboutWorldA[2] or answers.lower() == aboutWorldA[3] or answers.lower() == aboutWorldA[4]:
-        print("Correct")
-
+    global points
+    # if answers.lower() == aboutWorldA[0] or answers.lower() == aboutWorldA[1] or answers.lower() == aboutWorldA[2] or answers.lower() == aboutWorldA[3] or answers.lower() == aboutWorldA[4]:
+    #     print("Correct")
+    if answers.lower() == aboutWorldA[0]:
+        print("Correct!")
+        points = + 1
+    elif answers.lower() == aboutWorldA[1]:
+        print("Correct!")
+        points = + 1
+    elif answers.lower() == aboutWorldA[2]:
+        print("Correct!")
+        points = + 1
+    elif answers.lower() == aboutWorldA[3]:
+        print("Correct!")
+        points = + 1
+    elif answers.lower() == aboutWorldA[4]:
+        print("Correct!")
+        points = + 1
     else:
         print("Wrong!")
-        global points
         points = - 1
-
+    return points
 
 
 play = input("\nDo you want to play? ")
@@ -60,3 +74,4 @@ if play == "Yes" or "yes":
         answer(que5)
 
         print(f"\nAnswers: \nA-1: {aboutWorldA[0]}, A-2: {aboutWorldA[1]}, A-3: {aboutWorldA[2]}, A-4: {aboutWorldA[3]}, A-5: {aboutWorldA[4]}")
+        print(points)
