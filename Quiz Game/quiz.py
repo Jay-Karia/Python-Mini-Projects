e30b118d170cd8ea381525fa6c354cf7a06177be
Jay <86977383+Jay-Karia@users.noreply.github.com>
2021-07-29 13:20:23 +0530

    update quiz.py

--- Quiz Game/quiz.py
+++ Quiz Game/quiz.py
@@ -4,24 +4,28 @@ print("Welcome to Python Quiz Game Developed -- By Jay")
 topics = ["About World", "Current Affairs", "History", "Computer Science", "About Novels"]
 
 # Questions
-aboutWorldQ = ["Which one is the smallest ocean in the World?", "Which country gifted the 'Statue of Liberty' to USA in 1886?",
-               " or the first 500 million years after it formed?", "How did life begin",
-               "How does Earth's interior work, and how does it affect the surface?",
-               "Why does Earth have plate tectonics and continents?"]
+aboutWorldQ = ["Which one is the smallest ocean in the World?",
+               "Which country gifted the 'Statue of Liberty' to USA in 1886?",
+               "Dead Sea is located between which two countries?",
+               "In which ocean 'Bermuda Triangle' region is located?", "Which country is known as playground of europe"]
 
 # Answers
-aboutWorldA = ["Central Arctic Ocean", "French People"]
+aboutWorldA = ["Central Arctic Ocean", "French People", "Israel and Jordan", "North Atlantic Ocean", "Switzerland"]
 
 points = 0
 
 
 # Methods
-def answer(*answers):
-    if answers == "Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean" or "France" or "france" or "French People" or "french people":
+def answer(answers):
+    # lower = answers.lower()
+    if answers.lower() == "arctic ocean" or "central arctic ocean" or "france" or "french people" or "israel and jordan" or "switzerland":
+        print("Correct")
         global points
         points = + 1
-        print("Correct")
-        pass
+        return points
+
+    else:
+        print("Wrong!")
 
 
 play = input("\nDo you want to play? ")
@@ -33,10 +37,27 @@ if play == "Yes" or "yes":
     if select == "1":
         print("Questions ", topics[0])
 
-        print("\nQ-1: ", aboutWorldQ[0])
-        que1 = input()
-        answer("Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean")
+        for i in range(5):
+            que = input(f"\nQ-{i + 1}: {aboutWorldQ[i]}: ")
+            answer(que)
+        print(points)
 
-        print("\nQ-2: ", aboutWorldQ[1])
-        que2 = input()
-        answer("France" or "france" or "French People" or "french people")
+# print("\nQ-1: ", aboutWorldQ[0])
+# que1 = input()
+# answer("Arctic Ocean" or "Central Arctic Ocean" or "arctic ocean" or "central arctic ocean")
+#
+# print("\nQ-2: ", aboutWorldQ[1])
+# que2 = input()
+# answer("France" or "france" or "French People" or "french people")
+#
+# print("\nQ-3: ", aboutWorldQ[2])
+# que3 = input()
+# answer("Israel and Jordan", "israel and jordan")
+#
+# print("\nQ-3: ", aboutWorldQ[3])
+# que4 = input()
+# answer("north atlantic ocean", "North Atlantic Ocean")
+#
+# print("\nQ-3: ", aboutWorldQ[4])
+# que5 = input()
+# answer("switzerland", "Switzerland")
